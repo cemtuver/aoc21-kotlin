13e44563c0843a12bcbabe60e3b7ead697dd6e03
Cem Tuver <cemtuver@outlook.com>
2021-12-07 19:19:29 +0100

    Rework on day-06

--- day-06/part-2.kt
+++ day-06/part-2.kt
@@ -1,39 +1,28 @@
 import java.io.File
 
-const val SIMULATION_DAY = 256
-const val FISH_SPAWN_INTERVAL = 6
-const val FISH_SPAWN_INTERVAL_DELAY = 2
-
-fun setFishSpawnDays(startDay: Int, spawnFishCount: Long, spawnDayFisCountMap: MutableMap<Int, Long>) {
-    var spawnDay = startDay
-
-    while (spawnDay <= SIMULATION_DAY) {
-        spawnDayFisCountMap[spawnDay] = (spawnDayFisCountMap[spawnDay] ?: 0) + spawnFishCount
-        spawnDay += FISH_SPAWN_INTERVAL + 1
-    }
-}
-
 fun main(args: Array<String>) {
     val inputList = File("input.txt").readLines()
-    val initialSpawnDayList = inputList.first().split(",").map { it.toInt() }
-    val spawnDayFisCountMap = mutableMapOf<Int, Long>()
-    var numberOfFish = initialSpawnDayList.count().toLong()
+    val spawnDayCountMap = mutableMapOf<Int, Long>()
+    val initialFishList = inputList.first().split(",")
+        .map { it.toInt() }
 
-    initialSpawnDayList.forEach {
-        setFishSpawnDays(it + 1, 1, spawnDayFisCountMap)
+    initialFishList.forEach { fish ->
+        spawnDayCountMap[fish] = (spawnDayCountMap[fish] ?: 0) + 1
     }
 
-    for (i in 1 until SIMULATION_DAY + 1) {
-        val newFishCount = spawnDayFisCountMap[i] ?: continue
+    for (day in 1..256) {
+        val newFishCount = spawnDayCountMap[0] ?: 0
 
-        setFishSpawnDays(
-            i + FISH_SPAWN_INTERVAL + FISH_SPAWN_INTERVAL_DELAY + 1, 
-            newFishCount, 
-            spawnDayFisCountMap
-        )
+        for (i in 1..8) {
+            spawnDayCountMap[i - 1] = spawnDayCountMap[i] ?: 0
+        }
 
-        numberOfFish += newFishCount
+        spawnDayCountMap[6] = (spawnDayCountMap[6] ?: 0) + newFishCount
+        spawnDayCountMap[8] = newFishCount
     }
+
+    val totalFishCount = spawnDayCountMap.map { it.value }
+        .sum()
     
-    println(numberOfFish)
+    println(totalFishCount)
 }
