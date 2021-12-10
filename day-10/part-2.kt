d50f28965f09216613270741cebc19670d59a78c
Cem Tuver <cemtuver@outlook.com>
2021-12-10 20:41:11 +0100

    Implement day-10

---
+++ day-10/part-2.kt
@@ -0,0 +1,43 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    val inputList = File("input.txt").readLines()
+    val openChars = "([{<"
+    val closeChars = ")]}>"
+    val pointsMap = mapOf(
+        ')' to 1L,
+        ']' to 2L,
+        '}' to 3L,
+        '>' to 4L,
+    )
+
+    val scoreList = mutableListOf<Long>()
+
+    inputList.forEach lineLoop@{ line ->
+        var sum = 0L
+        val openCharList = mutableListOf<Char>()
+        val closeCharList = mutableListOf<Char>()
+
+        line.forEach charLoop@{ char -> 
+            when (char) {
+                in openChars -> openCharList.add(char)
+                in closeChars -> {
+                    val openChar = openCharList.removeAt(openCharList.count() - 1)
+
+                    if (openChars.indexOf(openChar) != closeChars.indexOf(char)) {
+                        return@lineLoop                    
+                    }
+                }
+            }
+        }
+
+        openCharList.asReversed().forEach { openChar ->
+            sum *= 5L
+            sum += pointsMap[closeChars[openChars.indexOf(openChar)]] ?: 0L
+        }
+
+        scoreList.add(sum)
+    }
+
+    println(scoreList.sorted().run { get(size / 2) })
+}
